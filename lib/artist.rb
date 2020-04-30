2ddc3bf746eef3acfda4a12f4666baf521d313ad
Devone Austin <devone.austin@gmail.com>
2020-04-30 01:32:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,7 @@ require_relative '../lib/concerns/paramable'
 class Artist
   extend Memorable::ClassMethods
   include Paramable::InstanceMethods
-  
+
   attr_accessor :name
   attr_reader :songs
 
